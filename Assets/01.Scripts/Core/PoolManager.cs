00b711c642279b2380b9224db54b922fee7855e8
ehdbs28 <doyun5373@naver.com>
2022-09-25 00:47:36 +0900

    Update PoolManager.cs

--- Assets/01.Scripts/Core/PoolManager.cs
+++ Assets/01.Scripts/Core/PoolManager.cs
@@ -2,17 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PoolManager : MonoBehaviour
+public class PoolManager : MonoSingleton<PoolManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    private Dictionary<string, Stack<GameObject>> _pools = new Dictionary<string,  Stack<GameObject>>();
+
+    private GameObject _prefab;
+    private Transform _trmParent;
+
+    public void CreatePool(GameObject prefab, Transform parent, int cnt = 10)
     {
-        
+        _prefab = prefab;
+        _trmParent = parent;
+        _pools.Add(_prefab.name, new Stack<GameObject>());
+        for (int i = 0; i < cnt; i++)
+        {
+            GameObject obj;
+            obj = GameObject.Instantiate (_prefab, _trmParent);
+            obj.gameObject.name = obj.gameObject.name.Replace("(Clone)", "");
+            obj.SetActive(false);
+            _pools[obj.name].Push(obj);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject Pop(string prefabName)
     {
-        
+        if(_pools.ContainsKey(prefabName) == false)
+        {
+            Debug.LogError("Obj hasent exist in PoolList");
+        }
+
+        GameObject obj = null;
+        if(_pools[prefabName].Count <= 0)
+        {
+            obj = GameObject.Instantiate(_prefab, _trmParent);
+            obj.name = obj.name.Replace("(Clone)", "");
+        }
+        else
+        {
+            obj = _pools[prefabName].Pop();
+            obj.SetActive(true);
+        }
+        return obj;
+    }
+
+    public void Push(GameObject obj)
+    {
+        obj.SetActive(false);
+        _pools[obj.name].Push(obj);
     }
 }

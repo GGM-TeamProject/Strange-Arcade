18c552bef012732a9f8401dc5edbeba0ca908a4a
도윤 <doyun5373@naver.com>
2022-11-16 21:23:16 +0900

    RollObject

--- Assets/01.Scripts/Stage2/RollObject.cs
+++ Assets/01.Scripts/Stage2/RollObject.cs
@@ -5,6 +5,9 @@ using DG.Tweening;
 
 public class RollObject : MonoBehaviour
 {
+    [SerializeField] private float _rollCoolTime = 30f;
+    [SerializeField] private float _rollCoolDownValue = 5f;
+
     private bool _isRoll = false;
     public bool IsRoll => _isRoll;
 
@@ -15,13 +18,26 @@ public class RollObject : MonoBehaviour
         _rollObjectParent = GameObject.Find("Screen/Stages/Stage_2/RollObjects").transform;
     }
 
-    [ContextMenu("test")]
-    public void TurnObject(){
+    private void Start() {
+        StartCoroutine(TurnCoroutine());
+    }
+
+    IEnumerator TurnCoroutine(){
+        while(true){
+            TurnObject();
+            yield return new WaitForSeconds(_rollCoolTime);
+        }
+    }
+
+    private void TurnObject(){
         _isRoll = true;
-        float lateRotate = (int)transform.rotation.z;
+        float lateRotate = Mathf.Round(transform.rotation.eulerAngles.z);
         Sequence sq = DOTween.Sequence();
-        sq.Append(_rollObjectParent.DORotate(new Vector3(0, 0, lateRotate - 10), 0.5f));
-        sq.Append(_rollObjectParent.DORotate(new Vector3(0, 0, lateRotate + 90), 0.3f));
-        sq.OnComplete(() => _isRoll = false);
+        sq.Append(_rollObjectParent.DORotate(new Vector3(0, 0, lateRotate + 10), 0.5f));
+        sq.Append(_rollObjectParent.DORotate(new Vector3(0, 0, lateRotate - 90), 0.3f));
+        sq.OnComplete(() => {
+            _isRoll = false;
+            _rollCoolTime -= _rollCoolDownValue;
+        });
     }
 }

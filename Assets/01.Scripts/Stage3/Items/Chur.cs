524c5667fa58df9b74d0af57179054a6eab487b1
ehdbs28 <doyun5373@naver.com>
2022-11-22 08:37:36 +0900

    츄르 스크립트 완성

--- Assets/01.Scripts/Stage3/Items/Chur.cs
+++ Assets/01.Scripts/Stage3/Items/Chur.cs
@@ -6,6 +6,7 @@ using DG.Tweening;
 public class Chur : MonoBehaviour, Iitem
 {
     private List<SpriteRenderer> _churBombs = new List<SpriteRenderer>();
+    private bool _attackRoutineIsRunning = false;
 
     private void Awake() {
         foreach(Transform churBomb in GameObject.Find("Screen/Stages/Stage_3/ChurBomb").transform){
@@ -13,19 +14,22 @@ public class Chur : MonoBehaviour, Iitem
         }
     }
 
-    [ContextMenu("asdf")]
     public void OnUseItem()
     {
+        if(_attackRoutineIsRunning) return;
         StartCoroutine(ChurAttack());
     }
 
     IEnumerator ChurAttack(){
+        _attackRoutineIsRunning = true;
         for(int i = 0; i < _churBombs.Count; i++){
-            _churBombs[i].transform.DOScale((i == 0) ? 2.7f : 2f, 1f);
-            _churBombs[i].DOFade(1f, 1f);
+            _churBombs[i].transform.DOScale((i == 0) ? 2.7f : 2f, 0.5f);
+            _churBombs[i].DOFade(1f, 0.5f);
 
-            yield return new WaitForSeconds(0.3f);
+            yield return new WaitForSeconds(0.1f);
         }
+
+        yield return new WaitForSeconds(1f);
 
         for(int i = 0; i < _churBombs.Count; i++){
             _churBombs[i].DOFade(0f, 4f);
@@ -36,5 +40,6 @@ public class Chur : MonoBehaviour, Iitem
         for(int i = 0; i < _churBombs.Count; i++){
             _churBombs[i].transform.DOScale(0f, 0.1f);
         }
+        _attackRoutineIsRunning = false;
     }
 }

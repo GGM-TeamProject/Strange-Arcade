206dd8388e3ef35261a7184cd879fee7b67968ca
ehdbs28 <doyun5373@naver.com>
2022-11-23 00:01:09 +0900

    대쉬UI 작업

--- Assets/01.Scripts/Stage3/Player/Stage3_Car.cs
+++ Assets/01.Scripts/Stage3/Player/Stage3_Car.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.VFX;
 using DG.Tweening;
+using UnityEngine.UI;
+using TMPro;
 
 public class Stage3_Car : MonoBehaviour, IDamage
 {
@@ -23,12 +25,20 @@ public class Stage3_Car : MonoBehaviour, IDamage
     [SerializeField] private float _dashDuration = 5f;
     [SerializeField] private float _dashCool = 5f;
     
-    [SerializeField]private float _dashGauge = 0f;
+    private float _dashGauge = 0f;
     private bool _isDash = false;
     private float _currentTime = 0f;
 
+    private Image _dashValue;
+    private TextMeshProUGUI _speedText;
+
     public float PlayerSpeed => _speed;
     public bool IsDash => _isDash;
+
+    private void Awake() {
+        _dashValue = transform.parent.Find("Stage3Canvas/DashGauge/BackGround/ValueImage").GetComponent<Image>();
+        _speedText = transform.parent.Find("Stage3Canvas/SpeedText").GetComponent<TextMeshProUGUI>();
+    }
 
     private void Update() {
         LineEffect();
@@ -41,6 +51,7 @@ public class Stage3_Car : MonoBehaviour, IDamage
     private void DashCoolTime(){
         _currentTime += Time.deltaTime;
         _dashGauge = Mathf.Lerp(0f, 100f, _currentTime / _dashCool);
+        _dashValue.fillAmount = Mathf.Lerp(0f, 1f, _currentTime / _dashCool);
         if(_currentTime >= _dashCool){
             if(Input.GetKeyDown(KeyCode.LeftShift)){
                 _isDash = true;
@@ -52,15 +63,27 @@ public class Stage3_Car : MonoBehaviour, IDamage
 
     private IEnumerator DashCoroutine(){
         _speed = _dashSpeed;
+
         _speedLine.SetGradient("ColorGradient", _neonGradient);
         _speedLine.SetFloat("SpawnRate", 256);
         _speedLine.Play();
+
         transform.DOMoveZ(20f, 0.5f);
-        //게이지 UI 줄이기
+
+        _speedText.text = $"{(int)_speed}\n        KM/H";
+        _speedText.DOColor(Color.red, 0.5f);
+        _speedText.rectTransform.DOShakeRotation(_dashDuration, 30, 10, 45);
+        _dashValue.DOFillAmount(0f, 0.5f);
+
         yield return new WaitForSeconds(_dashDuration);
+
         _speedLine.SetFloat("SpawnRate", 128);
         _speedLine.SetGradient("ColorGradient", _normalGradient);
         transform.DOMoveZ(17f, 0.5f);
+
+        _speedText.DOColor(Color.white, 0.5f);
+        _speedText.rectTransform.rotation = Quaternion.Euler(0, 0, -5);
+
         _isDash = false;
     }
 
@@ -72,6 +95,7 @@ public class Stage3_Car : MonoBehaviour, IDamage
             _speed -= Time.deltaTime * _deAccelPower;
         }
 
+        _speedText.text = $"{(int)_speed}\n        KM/H";
         _speed = Mathf.Clamp(_speed, 0f, 100f);
     }
 

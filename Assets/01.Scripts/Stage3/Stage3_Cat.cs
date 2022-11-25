bc2428987bb3fe2f9713c6c4469abb957bbc4d0d
도윤 <doyun5373@naver.com>
2022-11-26 01:10:50 +0900

    고양이 모델링 적용

--- Assets/01.Scripts/Stage3/Stage3_Cat.cs
+++ Assets/01.Scripts/Stage3/Stage3_Cat.cs
@@ -8,13 +8,16 @@ public class Stage3_Cat : MonoBehaviour
     [SerializeField] private float _moveMinValue = 0f;
     [SerializeField] private float _moveMaxValue = 0f;
     [SerializeField] private float _sapwnDelay = 3f;
+    [SerializeField] private Stage3_Car _player;
 
     private bool _isSpawnItem = false;
-    //private Animator _anim;
-    [SerializeField] private Stage3_Car _player;
+    private Transform _model;
+    private Animator _anim;
     private Vector3 _moveDir = Vector3.right;
 
     public void Init(){
+        _model = transform.Find("Model");
+        _anim = _model.GetComponent<Animator>();
         StartCoroutine(UpdatePath());
     }
 
@@ -23,6 +26,7 @@ public class Stage3_Cat : MonoBehaviour
     }
 
     private void Move(){
+        _anim.SetBool("IsMove", !_isSpawnItem);
         if(_isSpawnItem == false){
             transform.position += _moveDir * _moveSpeed * Time.deltaTime;
             if(transform.position.x <= _moveMinValue || transform.position.x >= _moveMaxValue){
@@ -37,6 +41,7 @@ public class Stage3_Cat : MonoBehaviour
             yield return new WaitForSeconds(_sapwnDelay);
 
             _isSpawnItem = true;
+            _anim.SetTrigger("IsThrow");
             Vector3 _spawnPos = new Vector3(transform.position.x, -11, 55);
             GameObject item = SetRandomItem(randPercentage);
             item.transform.position = _spawnPos;
